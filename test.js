0c2503ecaadf378491808507b1ee2565943cc5d3
Sergey Romanov <xxsmotur@gmail.com>
2015-05-28 23:55:39 +0500

    Start to add tests

---
+++ test.js
@@ -0,0 +1,28 @@
+var convo = require('./index.js');
+var assert = require("assert");
+
+
+describe('1D convolution with 1d kernel', function(){
+    it("should return one", function(){
+        assert.deepEqual(convo.conv1d([1],[1]),[1]);
+    });
+
+    it("should return 1,2,3", function(){
+        assert.deepEqual(convo.conv1d([1],[1,2,3]),[1,2,3]);
+    });
+
+    it("should return 70,140,210,280,350", function(){
+        assert.deepEqual(convo.conv1d([7],[10,20,30,40,50]),[70,140,210,280,350]);
+    });
+
+    it("should return 0,0,0,0,0", function(){
+        assert.deepEqual(convo.conv1d([0],[10,20,30,40,50]),[0,0,0,0,0]);
+    });
+
+    it("should return 3.92,5.46", function(){
+        assert.deepEqual(convo.conv1d([1.4],[2.8,3.9]),[3.9199999999999995,5.46]);
+    });
+
+
+
+});
